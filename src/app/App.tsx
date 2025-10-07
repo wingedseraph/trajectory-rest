9cd00c12ad44299d5f5e02dc21f6fb3c7e0ddabd
wingedseraph <r89647@gmail.com>
2025-10-07 19:59:23 +0300

    chore(ui): replace skeleton with spinner

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,37 +3,23 @@ import { Provider } from "@/app/provider";
 import MapView from "@/features/MapView/MapView";
 import TableControl from "@/features/TableControl/TableControl";
 import TableView from "@/features/TableView/TableView";
-import { Skeleton } from "@/shared/ui/Skeleton/skeleton";
+import { Spinner } from "@/shared/ui/Spinner/spinner";
 import Layout from "@/widgets/layout/layout";
 
 export default function App() {
   return (
     <Layout>
-      <div className="min-h-screen grid place-items-center p-4">
-        <Suspense
-          fallback={(
-            <div className="flex items-center space-x-4">
-              <Skeleton className="h-12 w-12 rounded-full" />
-              <div className="space-y-2">
-                <Skeleton className="h-4 sm:w-[250px] w-[160px]" />
-                <Skeleton className="h-4 sm:w-[200px] w-[120px]" />
-              </div>
-            </div>
+      <Suspense fallback={<Spinner />}>
+        <Provider>
+          {cars => (
+            <>
+              <TableControl />
+              <TableView cars={cars} />
+              <MapView />
+            </>
           )}
-        >
-          <Provider>
-            {cars => (
-              <div className="">
-                <TableControl />
-                <Suspense fallback={<Skeleton className="h-24 w-full" />}>
-                  <TableView cars={cars} />
-                </Suspense>
-                <MapView />
-              </div>
-            )}
-          </Provider>
-        </Suspense>
-      </div>
+        </Provider>
+      </Suspense>
     </Layout>
   );
 }
