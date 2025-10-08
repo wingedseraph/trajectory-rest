5684324c13971384fc11bffc99a01af17e99d42b
wingedseraph <r89647@gmail.com>
2025-10-08 12:23:05 +0300

    chore(spinner): remove unnecessary style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <Layout>
       <Suspense fallback={(
-        <div className="flex items-center justify-center h-screen w-screen">
+        <div className="flex items-center justify-center h-screen">
           <Spinner />
         </div>
       )}
