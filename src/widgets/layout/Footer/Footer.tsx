a9278dc0425019161f44f32edba3076b408d7804
wingedseraph <r89647@gmail.com>
2025-10-08 06:31:44 +0300

    chore(footer): replace div with footer to better semantic

--- src/widgets/layout/Footer/Footer.tsx
+++ src/widgets/layout/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="flex items-center justify-end gap-2">
+    <footer className="flex items-center justify-end gap-2">
       <a
         className="flex w-fit items-center justify-center text-xl"
         target="_blank"
@@ -9,6 +9,6 @@ export default function Footer() {
       >
         /github
       </a>
-    </div>
+    </footer>
   );
 }
